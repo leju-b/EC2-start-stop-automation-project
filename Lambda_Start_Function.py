5511265e629ff767100cf6057fcad9c4378698bb
Leju B <iamleju02@gmail.com>
2024-08-28 10:24:35 +0530

    changes made

--- Lambda_Start_Function.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    response = ec2.start_instances(
-        InstanceIds=['instance-id'],
-    )
-    print(f'Started EC2 instances: {response}')
--- Lambda_Stop_Function.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    response = ec2.stop_instances(
-        InstanceIds=['instance-id'],
-    )
-    print(f'Stopped EC2 instances: {response}')
